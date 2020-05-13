9b197743570ec0859249b0d07a67b0f06fef5900
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-13 02:47:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ end
 
 def countdown (number)
       while number > 0
-        puts "#{number} SECOND(S)!"
+        puts "#{number} SECOND(S)!".countdown_with_sleep
         number -= 1
       end
    "HAPPY NEW YEAR!"
