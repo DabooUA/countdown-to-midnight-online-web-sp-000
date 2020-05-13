c9a59db63d93c8703d125073da53014df9eba0f6
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-13 02:41:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown (number)
-    number = 10
+    
       while number >= 0 do
-        puts "\n#{number} SECOND(S)!"
+        puts "#{number} SECOND(S)!"
         number -= 1
       end
   puts "HAPPY NEW YEAR!"
